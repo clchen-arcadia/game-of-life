bc80cf81f6a0d121a5fcce9050ff5a965dff496f
Ezra Chung <ezra.chung97@gmail.com>
2023-04-26 13:25:05 -0700

    Functionality for grid and checking if neighbors are legal

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState }, React from "react";
 import { IGrid } from "./types";
 import Cell from "./Cell";
 
@@ -16,18 +16,59 @@ function Grid({nrows, ncols, chanceStartLive}: IGrid) {
         );
     }
 
+    function nextGenClick(): void{
+        //resetting the board
+        setGrid(oldGrid => {
+            for(let i = 0; i < oldGrid.length; i++){
+                for(let j = 0; j < oldGrid[i].length; j++){
+                    const legalNeighbors = [
+                                                [i-1,j],
+                                                [i+1,j],
+                                                [i,j-1],
+                                                [i,j+1],
+                                                [i-1,j-1],
+                                                [i-1,j+1],
+                                                [i+1,j-1],
+                                                [i+1,j+1],
+                                            ].filter(
+                                                c => (
+                                                    c[0] >= 0
+                                                    && c[0] < nrows
+                                                    && c[1] >= 0
+                                                    && c[1] < ncols
+                                                )
+                                            );
+                    const liveNeighbors = legalNeighbors.reduce(
+                        (liveNum, coord) => oldGrid[coord[0]][coord[1]] ? liveNum+1 : liveNum,
+                        0
+                    );
+                    if(liveNeighbors <= 1 || liveNeighbors >= 4) 
+                }
+            }
+        })
+    }
+
+    function getNewGrid(oldGrid:boolean[][]){
+        
+    }
 
     return (
-        <table className="Grid">
-            {grid.map(
-                row => <tr>
-                    {row.map(
-                        cellBool =>
-                        <Cell isLive={cellBool} />
-                    )}
-                </tr>
-            )}
-        </table>
+        <div>
+            <table className="Grid">
+                {grid.map(
+                    row => <tr>
+                        {row.map(
+                            cellBool =>
+                            <Cell isLive={cellBool} />
+                        )}
+                    </tr>
+                )}
+            </table>
+            <button className="nextGenButton" onClick={nextGenClick}>
+                NextGen
+            </button>
+        </div>
+
     )
 }
 
