b9feb1bc02a97f654b5b32c13fad3711748ce7c1
clchen-arcadia <clchen.arcadia@gmail.com>
2023-04-26 16:43:46 -0400

    Finish function to generate next generation

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -5,51 +5,73 @@ import Cell from "./Cell";
 /**
  *
  */
-function Grid({nrows, ncols, chanceStartLive}: IGrid) {
-    const [grid, setGrid] = useState(createBoard())
+function Grid({ nrows, ncols, chanceStartLive }: IGrid) {
+    const [grid, setGrid] = useState(createBoard());
 
     function createBoard(): boolean[][] {
-        return Array.from({length: nrows}).map(
-            row => Array.from({length: ncols}).map(
+        return Array.from({ length: nrows }).map(
+            row => Array.from({ length: ncols }).map(
                 cell => Math.random() < chanceStartLive
             )
         );
     }
 
-    function nextGenClick(): void{
+    function nextGenClick(): void {
         //resetting the board
         setGrid(oldGrid => {
-        })
+        });
     }
-    
-    function getNewGrid(oldGrid:boolean[][]){
-        for(let i = 0; i < oldGrid.length; i++){
-            for(let j = 0; j < oldGrid[i].length; j++){
+
+    function getNewGrid(oldGrid: boolean[][]): boolean[][] {
+        const newGrid = [];
+        
+        for (let i = 0; i < nrows; i++) {
+            const newRow = [];
+
+            for (let j = 0; j < ncols; j++) {
                 const legalNeighbors = [
-                                            [i-1,j],
-                                            [i+1,j],
-                                            [i,j-1],
-                                            [i,j+1],
-                                            [i-1,j-1],
-                                            [i-1,j+1],
-                                            [i+1,j-1],
-                                            [i+1,j+1],
-                                        ].filter(
-                                            c => (
-                                                c[0] >= 0
-                                                && c[0] < nrows
-                                                && c[1] >= 0
-                                                && c[1] < ncols
-                                            )
-                                        );
+                    [i - 1, j],
+                    [i + 1, j],
+                    [i, j - 1],
+                    [i, j + 1],
+                    [i - 1, j - 1],
+                    [i - 1, j + 1],
+                    [i + 1, j - 1],
+                    [i + 1, j + 1],
+                ].filter(
+                    c => (
+                        c[0] >= 0
+                        && c[0] < nrows
+                        && c[1] >= 0
+                        && c[1] < ncols
+                    )
+                );
+                let nextGenAlive = null;
+                const currAlive = oldGrid[i][j];
                 const liveNeighbors = legalNeighbors.reduce(
-                    (liveNum, coord) => oldGrid[coord[0]][coord[1]] ? liveNum+1 : liveNum,
+                    (liveNum, coord) => oldGrid[coord[0]][coord[1]] ? liveNum + 1 : liveNum,
                     0
                 );
-                if(liveNeighbors <= 1 || liveNeighbors >= 4) 
+                if (!currAlive) {
+                    if (liveNeighbors === 3) {
+                        nextGenAlive = true;
+                    } else {
+                        nextGenAlive = false;
+                    }
+                } else {
+                    if (liveNeighbors <= 1 || liveNeighbors >= 4) {
+                        nextGenAlive = false;
+                    } else {
+                        nextGenAlive = true;
+                    }
+                }
+                newRow.push(nextGenAlive);
             }
+
+            newGrid.push(newRow);
         }
-        
+
+        return newGrid;
     }
 
     return (
@@ -59,7 +81,7 @@ function Grid({nrows, ncols, chanceStartLive}: IGrid) {
                     row => <tr>
                         {row.map(
                             cellBool =>
-                            <Cell isLive={cellBool} />
+                                <Cell isLive={cellBool} />
                         )}
                     </tr>
                 )}
@@ -69,7 +91,7 @@ function Grid({nrows, ncols, chanceStartLive}: IGrid) {
             </button>
         </div>
 
-    )
+    );
 }
 
 export default Grid;
